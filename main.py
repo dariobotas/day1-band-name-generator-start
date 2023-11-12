e9355f68bdd7518de985cf56d6c9183a2aed471c
dariobotas <dario.botas@gmail.com>
2023-11-12 18:03:52 +0000

    day 1

--- main.py
+++ main.py
@@ -9,3 +9,21 @@ print("Your band name could be "+city_name+" "+pets_name)
 #5. Make sure the input cursor shows on a new line:
 
 # Solution: https://replit.com/@appbrewery/band-name-generator-end
+
+
+#If the bill was $150.00, split between 5 people, with 12% tip. 
+
+#Each person should pay (150.00 / 5) * 1.12 = 33.6
+#Format the result to 2 decimal places = 33.60
+
+#Tip: There are 2 ways to round a number. You might have to do some Googling to solve this.💪
+
+#Write your code below this line 👇
+
+bill = input("What was the total bill? ")
+tip = input("What was the tip you like to give? 10, 12 or 15? ")
+number_of_people = input("How many people to split the bill? ")
+
+payment_per_each_person = (bill / number_of_people) * (1 + tip / 100)
+
+print(round(payment_per_each_person, 2))
